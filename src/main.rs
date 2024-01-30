4d69213603711d85783760e49f49472a36fbb6b0
emurray2 <evan@auraaudio.io>
2024-01-29 23:29:29 -0500

    Add sample reading

--- src/main.rs
+++ src/main.rs
@@ -16,14 +16,27 @@ fn main() {
     // TODO: your code here
     let input_filename = &args[1];
     let output_filename = &args[2];
-    dbg!(input_filename);
-    dbg!(output_filename);
 
     // Open the input wave file and determine number of channels
     // TODO: your code here; see `hound::WavReader::open`.
     let path = Path::new(input_filename);
-    let reader = hound::WavReader::open(&path);
+    let mut reader = hound::WavReader::open(&path);
+    let existing_reader = reader.as_mut().unwrap();
     // Read audio data and write it to the output text file (one column per channel)
     // TODO: your code here; we suggest using `hound::WavReader::samples`, `File::create`, and `write!`.
     //       Remember to convert the samples to floating point values and respect the number of channels!
+    let samples = hound::WavReader::samples::<i16>((existing_reader));
+    for sample in samples {
+        let existing_sample = sample.unwrap() as f64;
+        let min = -32768.0;
+        let max = 32767.0;
+        let mut floating_sample = 0.0;
+        if existing_sample.is_sign_positive() {
+            floating_sample = existing_sample / max;
+        } else if existing_sample.is_sign_negative() {
+            floating_sample = (existing_sample / min) * -1.0;
+        } else {
+            floating_sample = 0.0;
+        }
+    }
 }
