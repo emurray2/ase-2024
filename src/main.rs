a2afe6ad54ab61ba05b848d4cd82394d4d69257d
emurray2 <evan@auraaudio.io>
2024-01-30 01:19:59 -0500

    Write file

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use std::path::Path;
 use std::{fs::File, io::Write};
+use std::fmt::Debug;
+use std::io::Read;
+use std::ptr::write;
 use hound::WavReader;
 
 fn show_info() {
@@ -20,12 +23,15 @@ fn main() {
     // Open the input wave file and determine number of channels
     // TODO: your code here; see `hound::WavReader::open`.
     let path = Path::new(input_filename);
+    let write_path = Path::new(output_filename);
     let mut reader = hound::WavReader::open(&path);
     let existing_reader = reader.as_mut().unwrap();
     // Read audio data and write it to the output text file (one column per channel)
     // TODO: your code here; we suggest using `hound::WavReader::samples`, `File::create`, and `write!`.
     //       Remember to convert the samples to floating point values and respect the number of channels!
     let samples = hound::WavReader::samples::<i16>((existing_reader));
+    let file = File::create(&write_path);
+    let mut existing_file = file.unwrap();
     for sample in samples {
         let existing_sample = sample.unwrap() as f64;
         let min = -32768.0;
@@ -38,5 +44,11 @@ fn main() {
         } else {
             floating_sample = 0.0;
         }
+        for i in 0..2 {
+            write!(existing_file, "{}", floating_sample).unwrap();
+            write!(existing_file, ",").unwrap();
+            continue
+        }
+        write!(existing_file, "\n").unwrap();
     }
 }
