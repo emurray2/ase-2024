7a31106a078b321a01387b3b478a34c4a35d4254
emurray2 <evan@auraaudio.io>
2024-02-20 22:16:19 -0500

    Add test mono and multichannel

--- src/comb_filter.rs
+++ src/comb_filter.rs
@@ -110,9 +110,8 @@ impl CombFilter {
 // TODO: feel free to define other types (here or in other modules) for your own use
 #[test]
 fn test_zero_output() {
-    let spec = WavSpec{channels: 1, sample_rate: 44100, bits_per_sample: 16, sample_format: SampleFormat::Int};
     let mut block_size: usize = 1024;
-    let channels = spec.channels as usize;
+    let channels = 1;
     let sample_rate = 44100.0;
     let frequency: f32 = 500.0;
     let delay = 1.0 / (frequency*2.0);
@@ -141,9 +140,8 @@ fn test_zero_output() {
 }
 #[test]
 fn test_magnitude_increase() {
-    let spec = WavSpec{channels: 1, sample_rate: 44100, bits_per_sample: 16, sample_format: SampleFormat::Int};
     let mut block_size: usize = 1024;
-    let channels = spec.channels as usize;
+    let channels = 1;
     let sample_rate = 44100.0;
     let frequency: f32 = 500.0;
     let delay = 1.0 / (frequency*2.0);
@@ -172,9 +170,8 @@ fn test_magnitude_increase() {
 }
 #[test]
 fn test_varying_block_size() {
-    let spec = WavSpec{channels: 1, sample_rate: 44100, bits_per_sample: 16, sample_format: SampleFormat::Int};
     let mut block_size: usize = 1024;
-    let channels = spec.channels as usize;
+    let channels = 1;
     let sample_rate = 44100.0;
     let frequency: f32 = 500.0;
     let delay = 1.0 / (frequency*2.0);
@@ -224,9 +221,8 @@ fn test_varying_block_size() {
 }
 #[test]
 fn test_correct_zero() {
-    let spec = WavSpec{channels: 1, sample_rate: 44100, bits_per_sample: 16, sample_format: SampleFormat::Int};
     let mut block_size: usize = 1024;
-    let channels = spec.channels as usize;
+    let channels = 1;
     let sample_rate = 44100.0;
     let frequency: f32 = 500.0;
     let delay = 1.0 / (frequency*2.0);
@@ -268,6 +264,53 @@ fn test_correct_zero() {
             if i > 50 {
                 let value = output_buffer[i][j];
                 assert!(f32::abs(value) <= f32::EPSILON);
+            }
+        }
+    }
+}
+#[test]
+fn test_mono_and_multichannel() {
+    let mut block_size: usize = 1024;
+    let channels= 1;
+    let multi_channels = 2;
+    let sample_rate = 44100.0;
+    let frequency: f32 = 500.0;
+    let delay = 1.0 / (frequency*2.0);
+    let mut comb_filter = CombFilter::new(IIR, delay, sample_rate, channels);
+    _ = comb_filter.set_param(Gain, 0.5);
+    let mut comb_filter_multi = CombFilter::new(IIR, delay, sample_rate, multi_channels);
+    _ = comb_filter_multi.set_param(Gain, 0.5);
+    let mut input_buffer = vec![vec![f32::default(); 1]; block_size];
+    let mut input_buffer_multi = vec![vec![f32::default(); 2]; block_size];
+    let mut output_buffer = vec![vec![f32::default(); 1]; block_size];
+    let mut output_buffer_multi = vec![vec![f32::default(); 2]; block_size];
+    for i in 0 .. block_size {
+        for j in 0 .. channels {
+            input_buffer[i][j] = (2.0*PI*frequency*(i as f32/sample_rate)).sin();
+        }
+    }
+    for i in 0 .. block_size {
+        for j in 0 .. multi_channels {
+            input_buffer_multi[i][j] = (2.0*PI*frequency*(i as f32/sample_rate)).sin();
+        }
+    }
+    let input_buffer_slice = &input_buffer[..].iter().map(|v| v.as_slice()).collect::<Vec<_>>();
+    let input_buffer_slice_2d = &input_buffer_slice[..];
+    let mut output_buffer_slice = output_buffer[..].iter_mut().map(|v| v.as_mut_slice()).collect::<Vec<_>>();
+    let output_buffer_slice_2d = &mut output_buffer_slice[..];
+    let input_buffer_slice_multi = &input_buffer_multi[..].iter().map(|v| v.as_slice()).collect::<Vec<_>>();
+    let input_buffer_slice_2d_multi = &input_buffer_slice_multi[..];
+    let mut output_buffer_slice_multi = output_buffer_multi[..].iter_mut().map(|v| v.as_mut_slice()).collect::<Vec<_>>();
+    let output_buffer_slice_2d_multi = &mut output_buffer_slice_multi[..];
+    comb_filter.process(input_buffer_slice_2d, output_buffer_slice_2d);
+    comb_filter_multi.process(input_buffer_slice_2d_multi, output_buffer_slice_2d_multi);
+    for i in 0 .. block_size {
+        for j in 0 .. multi_channels {
+            if i > 50 {
+                let value = output_buffer[i][0];
+                let multi_value = output_buffer_multi[i][j];
+                let channel_value = value - multi_value;
+                assert!(f32::abs(channel_value) <= f32::EPSILON);
             }
         }
     }
