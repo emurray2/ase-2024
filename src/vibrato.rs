82a122632e21f7a51aeac7706b1652fe9d5b764e
Evan Murray <evan@auraaudio.io>
2024-02-27 03:01:28 -0500

    Set params

--- src/vibrato.rs
+++ src/vibrato.rs
@@ -1,5 +1,7 @@
 /// Vibrato effect based on the DAFX Chapter 3 MATLAB implementation
 pub struct Vibrato {
+    #[doc(hidden)]
+    sample_rate: f32,
     /// The delay (width) of the vibrato in seconds
     pub delay_time: f32,
     #[doc(hidden)]
@@ -12,7 +14,9 @@ pub struct Vibrato {
 
 /// Parameters which characterize a Vibrato effect
 pub enum VibratoParam {
+    /// The delay (width) of the vibrato in seconds. Default value = 0.005 seconds (5 milliseconds).
     DelayTime,
+    /// The modulation frequency of the vibrato in Hertz. Default value = 0.1 Hertz.
     ModulationFrequency
 }
 
@@ -21,13 +25,26 @@ impl Vibrato {
     pub fn new(sample_rate: f32, delay_time: f32, modulation_frequency: f32) -> Self {
         let delay_length = (delay_time*sample_rate).round() as usize;
         let modulation_length = modulation_frequency/sample_rate;
-        Vibrato{delay_time,delay_length,modulation_frequency,modulation_length}
+        Vibrato{sample_rate,delay_time,delay_length,modulation_frequency,modulation_length}
+    }
+    /// Sets the parameter values for this effect. See [VibratoParam].
+    pub fn set_param(&mut self, param: VibratoParam, value: f32) {
+        match param {
+            VibratoParam::DelayTime => {
+                self.delay_time = value;
+                self.delay_length = (self.delay_time*self.sample_rate).round() as usize;
+            }
+            VibratoParam::ModulationFrequency => {
+                self.modulation_frequency = value;
+                self.modulation_length = self.modulation_frequency/self.sample_rate;
+            }
+        }
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::Vibrato;
+    use crate::{Vibrato, VibratoParam};
 
     #[test]
     fn test_initialization() {
@@ -36,5 +53,19 @@ mod tests {
         assert_eq!(effect.delay_length, 44100);
         assert_eq!(effect.modulation_frequency, 44100.0);
         assert_eq!(effect.modulation_length, 1.0);
+    }
+    #[test]
+    fn test_set_params() {
+        let mut effect = Vibrato::new(44100.0, 1.0, 44100.0);
+        assert_eq!(effect.delay_time, 1.0);
+        assert_eq!(effect.delay_length, 44100);
+        assert_eq!(effect.modulation_frequency, 44100.0);
+        assert_eq!(effect.modulation_length, 1.0);
+        effect.set_param(VibratoParam::DelayTime, 2.5);
+        assert_eq!(effect.delay_time, 2.5);
+        assert_eq!(effect.delay_length, 110250);
+        effect.set_param(VibratoParam::ModulationFrequency, 148.267);
+        assert_eq!(effect.modulation_frequency, 148.267);
+        assert_eq!(effect.modulation_length, 0.0033620635);
     }
 }
