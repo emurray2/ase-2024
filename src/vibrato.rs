e041eefc05b4a60ed50b274ad607e4132e05612d
emurray2 <evan@auraaudio.io>
2024-02-27 12:25:38 -0500

    Process

--- src/vibrato.rs
+++ src/vibrato.rs
@@ -45,10 +45,20 @@ impl Vibrato {
         self.set_param(VibratoParam::DelayTime, 0.005);
         self.set_param(VibratoParam::ModulationFrequency, 0.1);
     }
+    /// Process block for the effect. All the Digital Signal Processing happens here.
+    pub fn process(&mut self, input: &[&[f32]], output: &mut[&mut[f32]]) {
+        for (i, channel) in output.iter_mut().enumerate() {
+            for (j, y_n) in channel.iter_mut().enumerate() {
+                let x_n = input[i][j];
+                *y_n = x_n;
+            }
+        }
+    }
 }
 
 #[cfg(test)]
 mod tests {
+    use std::f32::consts::PI;
     use crate::{Vibrato, VibratoParam};
 
     #[test]
@@ -81,5 +91,30 @@ mod tests {
         assert_eq!(effect.delay_length, 221);
         assert_eq!(effect.modulation_frequency, 0.1);
         assert_eq!(effect.modulation_length, 0.000002267573696);
+    }
+    #[test]
+    fn test_process() {
+        let mut effect = Vibrato::new(44100.0, 1.0, 44100.0);
+        let channels = 2;
+        let block_size = 1024;
+        let frequency = 440.0;
+        let sample_rate = 44100.0;
+        let mut input_buffer = vec![vec![f32::default(); channels]; block_size];
+        let mut output_buffer = vec![vec![f32::default(); channels]; block_size];
+        for i in 0 .. block_size {
+            for j in 0 .. channels {
+                input_buffer[i][j] = (2.0*PI*440.0*(i as f32/sample_rate)).sin();
+            }
+        }
+        let input_buffer_slice = &input_buffer[..].iter().map(|v| v.as_slice()).collect::<Vec<_>>();
+        let input_buffer_slice_2d = &input_buffer_slice[..];
+        let mut output_buffer_slice = output_buffer[..].iter_mut().map(|v| v.as_mut_slice()).collect::<Vec<_>>();
+        let output_buffer_slice_2d = &mut output_buffer_slice[..];
+        effect.process(input_buffer_slice_2d, output_buffer_slice_2d);
+        for i in 0 .. block_size {
+            for j in 0 .. channels {
+                assert_eq!(output_buffer[i][j], input_buffer[i][j]);
+            }
+        }
     }
 }
