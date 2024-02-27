5b2e974a64ca2123eb2955c5a863da197d6cb4c0
Evan Murray <evan@auraaudio.io>
2024-02-27 02:02:34 -0500

    Modulation frequency

--- src/vibrato.rs
+++ src/vibrato.rs
@@ -1,14 +1,17 @@
 /// Vibrato effect based on the DAFX Chapter 3 MATLAB implementation
 pub struct Vibrato {
     /// Length of the delay in samples
-    delay_length: usize
+    delay_length: usize,
+    /// Length of the modulation in samples
+    modulation_length: f32
 }
 
 impl Vibrato {
-    /// Creates a Vibrato effect with the given sample rate and delay time in seconds.
-    pub fn new(sample_rate: f32, delay_time: f32) -> Self {
+    /// Creates a Vibrato effect with the given sample rate in sample, delay time in seconds, and modulation frequency in hertz.
+    pub fn new(sample_rate: f32, delay_time: f32, modulation_frequency: f32) -> Self {
         let delay_length = (delay_time*sample_rate).round() as usize;
-        Vibrato{delay_length}
+        let modulation_length = modulation_frequency/sample_rate;
+        Vibrato{delay_length,modulation_length}
     }
 }
 
@@ -18,7 +21,7 @@ mod tests {
 
     #[test]
     fn test_initialization() {
-        let effect = Some(Vibrato::new(44100.0, 5.0));
+        let effect = Some(Vibrato::new(44100.0, 5.0, 10.0));
         assert!(true)
     }
 }
