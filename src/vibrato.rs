2639514d35f959032ec8e5fc65fcaf2b430daccc
Evan Murray <evan@auraaudio.io>
2024-02-27 02:27:45 -0500

    Create public fields for params

--- src/vibrato.rs
+++ src/vibrato.rs
@@ -1,17 +1,26 @@
 /// Vibrato effect based on the DAFX Chapter 3 MATLAB implementation
 pub struct Vibrato {
-    /// Length of the delay in samples
+    /// The delay (width) of the vibrato in seconds
+    pub delay_time: f32,
+    #[doc(hidden)]
     delay_length: usize,
-    /// Length of the modulation in samples
-    modulation_length: f32
+    /// The modulation frequency of the vibrato in Hertz
+    pub modulation_frequency: f32,
+    #[doc(hidden)]
+    modulation_length: f32,
+}
+
+/// Parameters which characterize a Vibrato effect
+pub enum VibratoParam {
+
 }
 
 impl Vibrato {
-    /// Creates a Vibrato effect with the given sample rate in sample, delay time in seconds, and modulation frequency in hertz.
+    /// Creates a Vibrato effect with the given sample rate in samples, delay time, and modulation frequency
     pub fn new(sample_rate: f32, delay_time: f32, modulation_frequency: f32) -> Self {
         let delay_length = (delay_time*sample_rate).round() as usize;
         let modulation_length = modulation_frequency/sample_rate;
-        Vibrato{delay_length,modulation_length}
+        Vibrato{delay_time,delay_length,modulation_frequency,modulation_length}
     }
 }
 
@@ -21,7 +30,10 @@ mod tests {
 
     #[test]
     fn test_initialization() {
-        let effect = Some(Vibrato::new(44100.0, 5.0, 10.0));
-        assert!(true)
+        let effect = Vibrato::new(44100.0, 1.0, 44100.0);
+        assert_eq!(effect.delay_time, 1.0);
+        assert_eq!(effect.delay_length, 44100);
+        assert_eq!(effect.modulation_frequency, 44100.0);
+        assert_eq!(effect.modulation_length, 1.0);
     }
 }
