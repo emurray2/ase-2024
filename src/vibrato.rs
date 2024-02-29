ce0f3a4e43c556f0dab51d43af9a05e1153dc269
Evan Murray <evan@auraaudio.io>
2024-02-29 04:01:23 -0500

    Add modulator process

--- src/vibrato.rs
+++ src/vibrato.rs
@@ -1,3 +1,5 @@
+use crate::lfo::LFO;
+
 /// Vibrato effect based on the DAFX Chapter 3 MATLAB implementation
 pub struct Vibrato {
     #[doc(hidden)]
@@ -9,7 +11,7 @@ pub struct Vibrato {
     /// The modulation frequency of the vibrato in Hertz
     pub modulation_frequency: f32,
     #[doc(hidden)]
-    modulation_length: f32
+    modulator: LFO
 }
 
 /// Parameters which characterize a Vibrato effect
@@ -21,11 +23,10 @@ pub enum VibratoParam {
 }
 
 impl Vibrato {
-    /// Creates a Vibrato effect with the given sample rate in samples, delay time, and modulation frequency
-    pub fn new(sample_rate: f32, delay_time: f32, modulation_frequency: f32) -> Self {
+    /// Creates a Vibrato effect with the given sample rate in samples, delay time, modulation frequency, and number of channels
+    pub fn new(sample_rate: f32, delay_time: f32, modulation_frequency: f32, num_channels: usize) -> Self {
         let delay_length = (delay_time*sample_rate).round() as usize;
-        let modulation_length = modulation_frequency/sample_rate;
-        Vibrato{sample_rate,delay_time,delay_length,modulation_frequency,modulation_length}
+        Vibrato{sample_rate,delay_time,delay_length,modulation_frequency,modulator: LFO::new(modulation_frequency, 1.0, delay_length, num_channels, sample_rate)}
     }
     /// Sets the parameter values for this effect. See [VibratoParam].
     pub fn set_param(&mut self, param: VibratoParam, value: f32) {
@@ -36,7 +37,7 @@ impl Vibrato {
             }
             VibratoParam::ModulationFrequency => {
                 self.modulation_frequency = value;
-                self.modulation_length = self.modulation_frequency/self.sample_rate;
+                self.modulator.frequency = self.modulation_frequency;
             }
         }
     }
@@ -47,6 +48,12 @@ impl Vibrato {
     }
     /// Process block for the effect. All the Digital Signal Processing happens here.
     pub fn process(&mut self, input: &[&[f32]], output: &mut[&mut[f32]]) {
+        let block_size = input[0].len();
+        let num_channels = input.len();
+        let mut modulator_buffers = vec![vec![f32::default(); block_size]; num_channels];
+        let mut output_buffer_slice = modulator_buffers[..].iter_mut().map(|v| v.as_mut_slice()).collect::<Vec<_>>();
+        let output_buffer_slice_2d = &mut output_buffer_slice[..];
+        self.modulator.process(output_buffer_slice_2d);
         for (i, channel) in output.iter_mut().enumerate() {
             for (j, y_n) in channel.iter_mut().enumerate() {
                 let x_n = input[i][j];
@@ -63,47 +70,43 @@ mod tests {
 
     #[test]
     fn test_initialization() {
-        let effect = Vibrato::new(44100.0, 1.0, 44100.0);
+        let effect = Vibrato::new(44100.0, 1.0, 44100.0, 2);
         assert_eq!(effect.delay_time, 1.0);
         assert_eq!(effect.delay_length, 44100);
         assert_eq!(effect.modulation_frequency, 44100.0);
-        assert_eq!(effect.modulation_length, 1.0);
     }
     #[test]
     fn test_set_params() {
-        let mut effect = Vibrato::new(44100.0, 1.0, 44100.0);
+        let mut effect = Vibrato::new(44100.0, 1.0, 44100.0, 2);
         assert_eq!(effect.delay_time, 1.0);
         assert_eq!(effect.delay_length, 44100);
         assert_eq!(effect.modulation_frequency, 44100.0);
-        assert_eq!(effect.modulation_length, 1.0);
         effect.set_param(VibratoParam::DelayTime, 2.5);
         assert_eq!(effect.delay_time, 2.5);
         assert_eq!(effect.delay_length, 110250);
         effect.set_param(VibratoParam::ModulationFrequency, 148.267);
         assert_eq!(effect.modulation_frequency, 148.267);
-        assert_eq!(effect.modulation_length, 0.0033620635);
     }
     #[test]
     fn test_reset() {
-        let mut effect = Vibrato::new(44100.0, 1.0, 44100.0);
+        let mut effect = Vibrato::new(44100.0, 1.0, 44100.0, 2);
         effect.reset();
         assert_eq!(effect.delay_time, 0.005);
         assert_eq!(effect.delay_length, 221);
         assert_eq!(effect.modulation_frequency, 0.1);
-        assert_eq!(effect.modulation_length, 0.000002267573696);
     }
     #[test]
     fn test_process() {
-        let mut effect = Vibrato::new(44100.0, 1.0, 44100.0);
         let channels = 2;
         let block_size = 1024;
         let frequency = 440.0;
         let sample_rate = 44100.0;
-        let mut input_buffer = vec![vec![f32::default(); channels]; block_size];
-        let mut output_buffer = vec![vec![f32::default(); channels]; block_size];
-        for i in 0 .. block_size {
-            for j in 0 .. channels {
-                input_buffer[i][j] = (2.0*PI*frequency*(i as f32/sample_rate)).sin();
+        let mut effect = Vibrato::new(sample_rate, 1.0, 44100.0, channels);
+        let mut input_buffer = vec![vec![f32::default(); block_size]; channels];
+        let mut output_buffer = vec![vec![f32::default(); block_size]; channels];
+        for i in 0 .. channels {
+            for j in 0 .. block_size {
+                input_buffer[i][j] = (2.0*PI*frequency*(j as f32/sample_rate)).sin();
             }
         }
         let input_buffer_slice = &input_buffer[..].iter().map(|v| v.as_slice()).collect::<Vec<_>>();
@@ -111,8 +114,8 @@ mod tests {
         let mut output_buffer_slice = output_buffer[..].iter_mut().map(|v| v.as_mut_slice()).collect::<Vec<_>>();
         let output_buffer_slice_2d = &mut output_buffer_slice[..];
         effect.process(input_buffer_slice_2d, output_buffer_slice_2d);
-        for i in 0 .. block_size {
-            for j in 0 .. channels {
+        for i in 0 .. channels {
+            for j in 0 .. block_size {
                 assert_eq!(output_buffer[i][j], input_buffer[i][j]);
             }
         }
